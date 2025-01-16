da248ab2d450225eeb5b02fe652e7c286ad3ea3e
nathanielnyema <nyema@seas.upenn.edu>
2025-01-16 09:34:38 -0800

    cue arms on exiting stem for non-match to position

--- TMAZE_RPI_BEAMS/protocols/non_match_position.py
+++ TMAZE_RPI_BEAMS/protocols/non_match_position.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pyBehavior.protocols import Protocol
 from pyBehavior.gui import LoggableLineEdit, Parameter
 import numpy as np
+from multiprocessing.pool import ThreadPool
 
 class non_match_position(Protocol):
 
@@ -17,18 +18,18 @@ class non_match_position(Protocol):
     a_reward= State("a_reward", enter = "cue_stem")
     a_no_reward = State("a_no_reward", enter = "cue_stem")
     a_small_reward = State("a_small_reward", enter = "cue_stem")
+    a_baited = State("a_baited")
 
     b_reward= State("b_reward", enter = "cue_stem")
     b_no_reward = State("b_no_reward", enter = "cue_stem")
     b_small_reward = State("b_small_reward", enter = "cue_stem")
+    b_baited = State("b_baited")
 
     wandering = State("wandering")
 
-    beamA =  ( stem_reward.to(a_reward, cond="correct_arm",  after = "deliver_reward") 
-               | stem_reward.to(a_no_reward, cond="incorrect_arm") 
-               | stem_small_reward.to(a_reward, cond="correct_arm",  after = "deliver_reward") 
-               | stem_small_reward.to(a_no_reward, cond="incorrect_arm") 
-               | b_no_reward.to(a_small_reward, cond = "correct_arm",  after = "deliver_small_reward")
+    beamA =  ( a_baited.to(a_reward, before = "deliver_reward", after = "log_correct") 
+               | b_baited.to(a_no_reward, after = "log_incorrect") 
+               | b_no_reward.to(a_small_reward, before = "deliver_small_reward", after = "log_correct")
                | b_reward.to(wandering) |  b_small_reward.to(wandering)
                | sleep.to(wandering) | wandering.to.itself()
                | a_reward.to.itself() 
@@ -36,12 +37,9 @@ class non_match_position(Protocol):
                | a_small_reward.to.itself()
     )
 
-
-    beamB =  ( stem_reward.to(b_reward, cond="correct_arm", after = "deliver_reward") 
-               | stem_reward.to(b_no_reward, cond="incorrect_arm") 
-               | stem_small_reward.to(b_reward, cond="correct_arm",  after = "deliver_reward") 
-               | stem_small_reward.to(b_no_reward, cond="incorrect_arm") 
-               | a_no_reward.to(b_small_reward, cond = "correct_arm",  after = "deliver_small_reward")
+    beamB =  ( b_baited.to(b_reward, before = "deliver_reward", after = "log_correct") 
+               | a_baited.to(b_no_reward, after = "log_incorrect") 
+               | a_no_reward.to(b_small_reward, after = "deliver_small_reward",after = "log_correct")
                | a_reward.to(wandering) | a_small_reward.to(wandering)
                | sleep.to(wandering) | wandering.to.itself() 
                | b_reward.to.itself() 
@@ -50,15 +48,27 @@ class non_match_position(Protocol):
     )
 
     beamS =  ( a_reward.to(stem_reward,  after =  "toggle_target") 
-               | a_no_reward.to(stem_small_reward,  after =  "toggle_target") 
+               | a_no_reward.to(stem_reward,  after =  "toggle_target") 
                | a_small_reward.to(stem_small_reward,  after = "toggle_target")
                | b_reward.to(stem_reward,  after = "toggle_target") 
-               | b_no_reward.to(stem_small_reward,  after =  "toggle_target") 
+               | b_no_reward.to(stem_reward,  after =  "toggle_target") 
                | b_small_reward.to(stem_small_reward,  after =  "toggle_target") 
                | wandering.to(stem_small_reward,  after =  "toggle_target") 
                | sleep.to(stem_reward,  after =  "toggle_target")
                | stem_reward.to.itself() 
                | stem_small_reward.to.itself()
+               | a_baited.to.itself()
+               | b_baited.to.itself()
+    )
+
+    beamS2 = ( stem_reward.to(a_baited, cond = 'a_next', after = 'cue_target')
+               | stem_reward.to(b_baited, after = 'cue_target')
+               | stem_small_reward.to(a_baited, cond = 'a_next', after = 'cue_target')
+               | stem_small_reward.to(b_baited, after = 'cue_target')
+               | a_baited.to.itself() | b_baited.to.itself()
+               | a_reward.to.itself() | a_no_reward.to.itself() | a_small_reward.to.itself()
+               | b_reward.to.itself() | b_no_reward.to.itself() | b_small_reward.to.itself()
+               | wandering.to.itself() | sleep.to.itself()
     )
 
 
@@ -67,47 +77,73 @@ class non_match_position(Protocol):
         self.init = False
         self.beams = pd.Series({'beam8': self.beamB, 
                                 'beam16': self.beamA, 
-                                'beam17': self.beamS })
+                                'beam17': self.beamS,
+                                'beam18': self.beamS2})
         self.tracker = tracker(self)
         self.tracker.show()
         self.cue_stem()
+        self.thread_pool = ThreadPool(processes=5)
 
+    
+    @property
+    def is_probe(self):
+        return (self.tracker.trial_count.val %2) == 1
 
-    def correct_arm(self, event_data):
-        correct = self.target == event_data.target.id[0]
-        if correct:
-            if "small" in event_data.target.id:
-                self.parent.log(f"arm {event_data.target.id[0]} correct but initially incorrect")
-            else:
-                self.parent.log(f"arm {event_data.target.id[0]} correct")
-                self.tracker.correct_outbound.val += 1
-        return correct
+    def a_next(self):
+        return self.target == 'a'
     
     def clear_leds(self):
         for i in self.parent.reward_modules:
-            mod = self.parent.reward_modules[i].module
-            led_state = bool(self.parent.client.get(f"modules['{mod}'].LED.on"))
-            if led_state:
-                self.parent.reward_modules[i].toggle_led()
+            self.turn_off_led(i)
 
-    def cue_arm(self, arm):
-        self.clear_leds()
+    def cue_arm(self, arm, timeout = None):
         mod = self.parent.reward_modules[arm]
         led_state = bool(self.parent.client.get(f"modules['{mod.module}'].LED.on"))
         if not led_state:
             mod.toggle_led()
+            if timeout is not None:
+                self.thread_pool.apply(self.turn_off_led, (arm, timeout))
+
+    def turn_off_led(self, arm, delay=None):
+        if delay is not None:
+            time.sleep(delay)
+        mod = self.parent.reward_modules[arm]
+        led_state = bool(self.parent.client.get(f"modules['{mod.module}'].LED.on"))
+        if led_state:
+            mod.toggle_led()
     
     def cue_stem(self):
+        self.clear_leds()
         self.cue_arm('s')
-        
-    def incorrect_arm(self, event_data):
-        incorrect = self.target != event_data.target.id[0]
-        if incorrect:
-            self.parent.log(f"arm {event_data.target.id[0]} incorrect")
-        return incorrect
     
-    def toggle_target(self, event_data):
-        if "small" in event_data.target.id:
+    def cue_target(self):
+        cue = True
+        timeout=None
+        self.clear_leds()
+        if self.is_probe:
+            cue = self.tracker.cue_probe.isChecked():
+            if self.tracker.blink_probe_cue.isChecked():
+                timeout = float(self.tracker.probe_cue_blink_time.text())
+        if cue: self.cue_arm(self.target, timeout=timeout)
+        self.tracker.current_trial_start = datetime.now()
+        
+    def log_correct(self):
+        if 'small' in self.current_state.id:
+            self.parent.log(f"arm {self.current_state.id[0]} correct but initially incorrect")
+        else:
+            self.parent.log(f"arm {self.current_state.id[0]} correct")
+            self.tracker.correct_outbound.val += 1
+            if self.is_probe:
+                if self.target == 'a':
+                    self.tracker.correct_a_probes.val += 1
+                else:
+                    self.tracker.correct_b_probes.val += 1
+
+    def log_incorrect(self):
+        self.parent.log(f"arm {self.current_state.id[0]} incorrect")
+    
+    def toggle_target(self):
+        if "small" in self.current_state.id:
             self.parent.log(f"stem correct but initially incorrect")
             self.deliver_small_reward()
         else:
@@ -115,19 +151,11 @@ class non_match_position(Protocol):
             self.tracker.correct_inbound.val += 1
             self.deliver_reward()
         self.tracker.trial_count.val += 1
-        is_probe = (self.tracker.trial_count.val %2) == 1
-        cue = True
-        if is_probe:
+        if self.is_probe:
             self.target = 'b' if self.target=='a' else 'a'
-            cue = self.tracker.cue_probe.isChecked()
         else:
             self.target = ['a','b'][np.random.choice(2)]
         self.tracker.target.setText(f"{self.target}")
-        if cue: 
-            self.cue_arm(self.target)
-        else:
-            self.clear_leds()
-        self.tracker.current_trial_start = datetime.now()
 
     def deliver_reward(self):
         arm = self.current_state.id[0]
@@ -153,6 +181,7 @@ class tracker(QMainWindow):
         super(tracker, self).__init__()
         self.layout = QVBoxLayout()
         self.parent = parent
+
         reward_amount_layout = QHBoxLayout()
         reward_amount_label = QLabel("Reward Amount (mL): ")
         self.reward_amount = LoggableLineEdit("reward_amount", self.parent.parent)
@@ -177,19 +206,36 @@ class tracker(QMainWindow):
         self.cue_probe.stateChanged.connect(self.log_cue_probe)
         cue_probe_layout.addWidget(self.cue_probe)
 
+        blink_probe_cue_layout = QHBoxLayout()
+        cue_probe_layout.addWidget(QLabel("Blink Probe Cue:"))
+        self.blink_probe_cue = QCheckBox()
+        self.blink_probe_cue.setChecked(False)
+        self.blink_probe_cue.stateChanged.connect(self.log_cue_probe)
+        blink_probe_cue_layout.addWidget(self.blink_probe_cue)
+
+        blink_time_layout = QHBoxLayout()
+        blink_time_layout.addWidget(QLabel("Probe Trial Cue Blink Time [s]:"))
+        self.probe_cue_blink_time = LoggableLineEdit("c", self.parent.parent)
+        self.probe_cue_blink_time.setText("5")
+        self.probe_cue_blink_time.setValidator(QDoubleValidator())
+        blink_time_layout.addWidget(self.probe_cue_blink_time)
+
         settings = QGroupBox()
         slayout = QVBoxLayout()
         slayout.addLayout(reward_amount_layout)
         slayout.addLayout(small_rew_layout)
         slayout.addLayout(cue_probe_layout)
+        slayout.addLayour(blink_probe_cue_layout)
+        slayout.addLayour(blink_time_layout)
         settings.setLayout(slayout)
         self.layout.addWidget(settings)
-
 
         self.tot_rewards = Parameter("Total # Rewards", default = 0, is_num = True)
         self.total_reward = Parameter("Total Reward [mL]", default =  0, is_num = True)
         self.trial_count = Parameter("Trial Count", default=-1, is_num=True)
         self.correct_outbound = Parameter("# correct outbound", default=0, is_num=True)
+        self.correct_a_probes = Parameter("# correct a probes", default=0, is_num=True)
+        self.correct_b_probes = Parameter("# correct b probes", default=0, is_num=True)
         self.correct_inbound = Parameter("# correct inbound", default=0, is_num=True)
         self.current_state = Parameter("current state", default="*waiting to start*")
         self.target =  Parameter("target", default="no target")
@@ -204,6 +250,8 @@ class tracker(QMainWindow):
         ilayout = QVBoxLayout()
         ilayout.addWidget(self.trial_count)
         ilayout.addWidget(self.correct_outbound)
+        ilayout.addWidget(self.correct_a_probes)
+        ilayout.addWidget(self.correct_b_probes)
         ilayout.addWidget(self.correct_inbound)
         ilayout.addWidget(self.current_state)
         ilayout.addWidget(self.target)
@@ -221,6 +269,12 @@ class tracker(QMainWindow):
 
     def log_cue_probe(self, i):
         self.parent.parent.log(f"cue_probe set to {self.cue_probe.isChecked()}")
+        self.blink_probe_cue.setEnabled(self.cue_probe.isChecked())
+        self.probe_cue_blink_time.setEnabled(self.cue_probe.isChecked() and self.blink_probe_cue.isChecked())
+
+    def log_blink_probe_cue(self, i):
+        self.parent.parent.log(f"blink_probe_cue set to {self.blink_probe_cue.isChecked()}")
+        self.probe_cue_blink_time.setEnabled(self.cue_probe.isChecked() and self.blink_probe_cue.isChecked())
 
     def update_time(self):
         self.exp_time.setText(f"{(datetime.now() - self.t_start).total_seconds()/60:.2f}")
