4e6a6842c127199e589e37d0f11373888d079533
Nathaniel Nyema <nnyema@caltech.edu>
2025-02-11 15:01:42 -0800

    make probes occur randomly in non_match_position

--- TMAZE_RPI_BEAMS/protocols/non_match_position.py
+++ TMAZE_RPI_BEAMS/protocols/non_match_position.py
@@ -1,6 +1,6 @@
 from statemachine import State
 from PyQt5.QtCore import  QTimer
-from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QVBoxLayout, QWidget, QLabel, QGroupBox, QCheckBox
+from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QVBoxLayout, QWidget, QLabel, QGroupBox, QCheckBox, QButtonGroup, QRadioButton
 from PyQt5.QtGui import  QDoubleValidator
 from datetime import datetime
 import pandas as pd
@@ -14,7 +14,7 @@ class non_match_position(Protocol):
 
     sleep = State("sleep", initial=True)
     stem_reward= State("stem_reward")
-    stem_small_reward = State("stem_smzall_reward")
+    stem_small_reward = State("stem_small_reward")
 
     a_reward= State("a_reward")
     a_no_reward = State("a_no_reward")
@@ -40,7 +40,7 @@ class non_match_position(Protocol):
 
     beamB =  ( b_baited.to(b_reward, on = "deliver_reward", after = "log_correct") 
                | a_baited.to(b_no_reward, after = "log_incorrect") 
-               | a_no_reward.to(b_small_reward, on = "deliver_small_reward",after = "log_correct")
+               | a_no_reward.to(b_small_reward, on = "deliver_small_reward", after = "log_correct")
                | a_reward.to(wandering) | a_small_reward.to(wandering)
                | sleep.to(wandering) | wandering.to.itself() 
                | b_reward.to.itself() 
@@ -84,13 +84,21 @@ class non_match_position(Protocol):
         self.tracker.show()
         self.cue_stem()
         self.thread_pool = ThreadPool(processes=5)
-
-    
-    @property
-    def is_probe(self):
-        return (self.tracker.trial_count.val %2) == 0
+        self.target = None
+        self.prev_target = None
+        self.is_probe = True
 
     def a_next(self):
+        mode = self.tracker.mode.checkedButton().text()
+        if mode == 'Force A Cued':
+            self.target = 'a'
+            self.is_probe = False
+        elif mode == 'Force B Cued':
+            self.target = 'b'
+            self.is_probe = False
+        elif mode == 'Force Probe':
+            self.target = 'b' if self.prev_target =='a' else 'a'
+            self.is_probe = True
         return self.target == 'a'
     
     def clear_leds(self):
@@ -121,29 +129,25 @@ class non_match_position(Protocol):
         cue = True
         timeout=None
         self.clear_leds()
+        self.tracker.is_probe.setChecked(self.is_probe)
         if self.is_probe:
-            cue = self.tracker.cue_probe.isChecked()
-            if self.tracker.blink_probe_cue.isChecked():
+            self.parent.log("probe trial")
+            cue = not self.tracker.blink_probe_cue.isChecked()
+            if cue:
+                self.parent.log("blinking cue")
                 timeout = float(self.tracker.probe_cue_blink_time.text())
+        self.tracker.increment_trial_type(self.target, self.is_probe)
         if cue: self.cue_arm(self.target, timeout=timeout)
         self.tracker.current_trial_start = datetime.now()
+        self.prev_target = self.target
         
     def log_correct(self):
+        arm = self.current_state.id[0]
         if 'small' in self.current_state.id:
-            self.parent.log(f"arm {self.current_state.id[0]} correct but initially incorrect")
+            self.parent.log(f"arm {arm} correct but initially incorrect")
         else:
-            self.parent.log(f"arm {self.current_state.id[0]} correct")
-            self.tracker.correct_outbound.val += 1
-            if self.is_probe:
-                if self.target == 'a':
-                    self.tracker.correct_a_probes.val += 1
-                else:
-                    self.tracker.correct_b_probes.val += 1
-            else:
-                if self.target == 'a':
-                    self.tracker.correct_a_cued.val += 1
-                else:
-                    self.tracker.correct_b_cued.val += 1
+            self.parent.log(f"arm {arm} correct")
+            self.tracker.increment_correct(arm, self.is_probe)
             self.cue_stem()
 
     def log_incorrect(self):
@@ -161,20 +165,25 @@ class non_match_position(Protocol):
         else:
             self.deliver_reward(target)
         self.tracker.trial_count.val += 1
+        probe_prob =  float(self.tracker.probe_prob.text())
+        self.is_probe = False if self.is_probe else np.random.uniform() < probe_prob
         if self.is_probe:
-            self.target = 'b' if self.target=='a' else 'a'
-            if self.target == 'a':
-                self.tracker.a_probes.val += 1
-            else:
-                self.tracker.b_probes.val += 1
+            self.target = 'b' if self.prev_target == 'a' else 'a'
         else:
-            a_prob = float(self.tracker.a_prob.text())
-            self.target = ['a','b'][np.random.choice(2, p = [a_prob, 1-a_prob])]
-            if self.target == 'a':
-                self.tracker.a_cued.val += 1
+            if self.tracker.sticky.isChecked():
+                if self.target is None:
+                    a_prob = float(self.tracker.a_prob.text())
+                    p = [a_prob, 1-a_prob]
+                elif self.target == 'a':
+                    p_aa = float(self.tracker.p_aa.text())
+                    p = [p_aa, 1-p_aa]
+                else:
+                    p_bb = float(self.tracker.p_bb.text())
+                    p = [1-p_bb, p_bb]
             else:
-                self.tracker.b_cued.val += 1
-        self.tracker.target.setText(f"{self.target}")
+                a_prob = float(self.tracker.a_prob.text())
+                p = [a_prob, 1-a_prob]
+            self.target = ['a','b'][np.random.choice(2, p = p)]
 
     def deliver_reward(self, target):
         arm = target.id[0]
@@ -230,19 +239,20 @@ class tracker(QMainWindow):
         self.stem_small_rew_frac.setValidator(only_frac)
         stem_small_rew_layout.addWidget(stem_small_rew_label)
         stem_small_rew_layout.addWidget(self.stem_small_rew_frac)
-
-        cue_probe_layout = QHBoxLayout()
-        cue_probe_layout.addWidget(QLabel("Cue Probe Trials:"))
-        self.cue_probe = QCheckBox()
-        self.cue_probe.setChecked(True)
-        self.cue_probe.stateChanged.connect(self.log_cue_probe)
-        cue_probe_layout.addWidget(self.cue_probe)
+        
+        probe_prob_layout = QHBoxLayout()
+        probe_prob_layout.addWidget(QLabel("Probe Trial Probability [0-1]:"))
+        self.probe_prob = LoggableLineEdit("probe_prob", self.parent.parent)
+        only_frac = QDoubleValidator(0., 1., 6, notation = QDoubleValidator.StandardNotation)
+        self.probe_prob.setValidator(only_frac)
+        self.probe_prob.setText('0.1')
+        probe_prob_layout.addWidget(self.probe_prob)
 
         blink_probe_cue_layout = QHBoxLayout()
         blink_probe_cue_layout.addWidget(QLabel("Blink Probe Cue:"))
         self.blink_probe_cue = QCheckBox()
         self.blink_probe_cue.setChecked(False)
-        self.blink_probe_cue.stateChanged.connect(self.log_cue_probe)
+        self.blink_probe_cue.stateChanged.connect(self.log_blink_probe_cue)
         blink_probe_cue_layout.addWidget(self.blink_probe_cue)
 
         blink_time_layout = QHBoxLayout()
@@ -253,25 +263,85 @@ class tracker(QMainWindow):
         self.probe_cue_blink_time.setEnabled(False)
         blink_time_layout.addWidget(self.probe_cue_blink_time)
 
+        mode_layout = QHBoxLayout()
+        mode_layout.addWidget(QLabel("Mode:"))
+        self.mode = QButtonGroup()
+        rand = QRadioButton("Auto")
+        mode_layout.addWidget(rand)
+        rand.setChecked(True)
+        self.mode.addButton(rand, id=0)
+        force_a = QRadioButton("Force A Cued")
+        mode_layout.addWidget(force_a)
+        self.mode.addButton(force_a, id=1)
+        force_b = QRadioButton("Force B Cued")
+        mode_layout.addWidget(force_b)
+        self.mode.addButton(force_b, id=2)
+        force_probe = QRadioButton("Force Probe")
+        mode_layout.addWidget(force_probe)
+        self.mode.addButton(force_probe, id=3)
+
+        sticky_layout = QHBoxLayout()
+        sticky_layout.addWidget(QLabel("Specify Transition Probabilities:"))
+        self.sticky = QCheckBox()
+        self.sticky.setChecked(True)
+        self.sticky.stateChanged.connect(self.log_sticky)
+        sticky_layout.addWidget(self.sticky)
+
+        p_aa_layout = QHBoxLayout()
+        p_aa_layout.addWidget(QLabel("P(A|A):"))
+        self.p_aa = LoggableLineEdit("p_aa", self.parent.parent)
+        only_frac = QDoubleValidator(0., 1., 6, notation = QDoubleValidator.StandardNotation)
+        self.p_aa.setText("0.5")
+        self.p_aa.setValidator(only_frac)
+        self.p_aa.editingFinished.connect(self.update_a_prob)
+        p_aa_layout.addWidget(self.p_aa)
+
+        p_bb_layout = QHBoxLayout()
+        p_bb_layout.addWidget(QLabel("P(B|B):"))
+        self.p_bb = LoggableLineEdit("p_bb", self.parent.parent)
+        only_frac = QDoubleValidator(0., 1., 6, notation = QDoubleValidator.StandardNotation)
+        self.p_bb.setText("0.5")
+        self.p_bb.setValidator(only_frac)
+        self.p_bb.editingFinished.connect(self.update_a_prob)
+        p_bb_layout.addWidget(self.p_bb)
+
         a_prob_layout = QHBoxLayout()
         a_prob_layout.addWidget(QLabel("A Cued Probability [0-1]:"))
         self.a_prob = LoggableLineEdit("a_prob", self.parent.parent)
         only_frac = QDoubleValidator(0., 1., 6, notation = QDoubleValidator.StandardNotation)
-        self.a_prob.setText("0.5")
         self.a_prob.setValidator(only_frac)
+        self.update_a_prob()
+        self.a_prob.setEnabled(False)
         a_prob_layout.addWidget(self.a_prob)
+
+        probs = QGroupBox()
+        playout = QVBoxLayout()
+        playout.addLayout(sticky_layout)
+        playout.addLayout(p_aa_layout)
+        playout.addLayout(p_bb_layout)
+        playout.addLayout(a_prob_layout)
+        probs.setLayout(playout)
 
         settings = QGroupBox()
         slayout = QVBoxLayout()
         slayout.addLayout(reward_amount_layout)
         slayout.addLayout(small_rew_layout)
         slayout.addLayout(stem_small_rew_layout)
-        slayout.addLayout(cue_probe_layout)
+        slayout.addLayout(probe_prob_layout)
         slayout.addLayout(blink_probe_cue_layout)
         slayout.addLayout(blink_time_layout)
         slayout.addLayout(a_prob_layout)
+        slayout.addLayout(mode_layout)
+        slayout.addWidget(probs)
         settings.setLayout(slayout)
         self.layout.addWidget(settings)
+
+        is_probe_layout = QHBoxLayout()
+        is_probe_layout.addWidget(QLabel("Probe Trial:"))
+        self.is_probe = QCheckBox()
+        self.is_probe.setChecked(False)
+        self.is_probe.setEnabled(False)
+        is_probe_layout.addWidget(self.is_probe)
 
         self.tot_rewards = Parameter("Total # Rewards", default = 0, is_num = True)
         self.total_reward = Parameter("Total Reward [mL]", default =  0, is_num = True)
@@ -279,7 +349,6 @@ class tracker(QMainWindow):
         self.correct_outbound = Parameter("# correct outbound", default=0, is_num=True)
         self.correct_inbound = Parameter("# correct inbound", default=0, is_num=True)
         self.current_state = Parameter("current state", default="*waiting to start*")
-        self.target =  Parameter("target", default="no target")
         self.exp_time = Parameter(f"Experiment Time [min]", default = 0, is_num=True)
         self.current_trial_time = Parameter(f"Current Trial Time [s]", default = 0, is_num=True)
         self.t_start = datetime.now()
@@ -315,13 +384,13 @@ class tracker(QMainWindow):
 
         info = QGroupBox()
         ilayout = QVBoxLayout()
+        ilayout.addLayout(is_probe_layout)
         ilayout.addWidget(a)
         ilayout.addWidget(b)
         ilayout.addWidget(self.trial_count)
         ilayout.addWidget(self.correct_outbound)
         ilayout.addWidget(self.correct_inbound)
         ilayout.addWidget(self.current_state)
-        ilayout.addWidget(self.target)
         ilayout.addWidget(self.tot_rewards)
         ilayout.addWidget(self.total_reward)
         ilayout.addWidget(self.exp_time)
@@ -333,15 +402,26 @@ class tracker(QMainWindow):
         container.setLayout(self.layout)
         self.setCentralWidget(container)
         self.timer.start(1000)
-
-    def log_cue_probe(self, i):
-        self.parent.parent.log(f"cue_probe set to {self.cue_probe.isChecked()}")
-        self.blink_probe_cue.setEnabled(self.cue_probe.isChecked())
-        self.probe_cue_blink_time.setEnabled(self.cue_probe.isChecked() and self.blink_probe_cue.isChecked())
-
+    
     def log_blink_probe_cue(self, i):
         self.parent.parent.log(f"blink_probe_cue set to {self.blink_probe_cue.isChecked()}")
-        self.probe_cue_blink_time.setEnabled(self.cue_probe.isChecked() and self.blink_probe_cue.isChecked())
+        self.probe_cue_blink_time.setEnabled(self.blink_probe_cue.isChecked())
+
+
+    def log_sticky(self):
+        sticky = self.sticky.isChecked()
+        self.parent.parent.log(f"sticky set to {sticky}")
+        self.p_aa.setEnabled(sticky)
+        self.p_bb.setEnabled(sticky)
+        self.a_prob.setEnabled(not sticky)
+        if sticky: self.update_a_prob()
+
+    def update_a_prob(self):
+        p_aa = float(self.p_aa.text())
+        p_bb = float(self.p_bb.text())
+        p_ab = 1 - p_bb
+        p = p_ab/(1 - p_aa + p_ab)
+        self.a_prob.setText(f"{p}")
 
     def update_time(self):
         self.exp_time.setText(f"{(datetime.now() - self.t_start).total_seconds()/60:.2f}")
@@ -352,3 +432,28 @@ class tracker(QMainWindow):
             amount = float(self.reward_amount.text())
         self.tot_rewards.val += 1
         self.total_reward.val += amount
+    
+    def increment_trial_type(self, target, probe):
+        if probe:
+            if target == 'a':
+                self.a_probes.val += 1
+            else:
+                self.b_probes.val += 1
+        else:
+            if target == 'a':
+                self.a_cued.val += 1
+            else:
+                self.b_cued.val += 1
+
+    def increment_correct(self, target, probe):
+        self.correct_outbound.val += 1
+        if probe:
+            if target == 'a':
+                self.correct_a_probes.val += 1
+            else:
+                self.correct_b_probes.val += 1
+        else:
+            if target == 'a':
+                self.correct_a_cued.val += 1
+            else:
+                self.correct_b_cued.val += 1
